e6e6264d1648eff8bbe8e055928153065597dc79
BASARSOFT\hakan.beyhan <hakan.beyhan@basarsoft.com.tr>
2023-02-10 11:15:31 +0300

    düzenleme

--- src/test/java/testAutomations/seleniumTest/TS_0010_Analizler/TC_0010_05_Analizler_Hiyerarşik_Tüik_Sorgulama.java
+++ src/test/java/testAutomations/seleniumTest/TS_0010_Analizler/TC_0010_05_Analizler_Hiyerarşik_Tüik_Sorgulama.java
@@ -28,7 +28,7 @@ public class TC_0010_05_Analizler_Hiyerarşik_Tüik_Sorgulama extends testAutoma
     @FindBy(xpath = "//body/span[1]/span[1]/span[1]/input[1]")
     public WebElement tabloGirisi2;
 
-    @FindBy(xpath = "//button[@id='btn-run-tuik-polygon']")
+    @FindBy(xpath = "//body/div[@id='kt_quick_panel']/div[3]/div[1]/div[3]/div[1]/div[1]/div[3]/button[1]/i[1]")
     public WebElement getirButonu;
 
 
