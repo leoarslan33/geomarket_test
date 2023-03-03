eef5d1f4f7433fc16f906eefb0faeef68d770876
BASARSOFT\mert.sen <mert.sen@basarsoft.com.tr>
2023-03-03 15:23:18 +0300

    hta çözüldü

--- src/test/java/testAutomations/seleniumTest/TS_0013_Haritaya_Bölge_Çiz_İşlemleri/TC_0013_07_Haritaya_Bölge_Çiz_İşlemi_Sil.java
+++ src/test/java/testAutomations/seleniumTest/TS_0013_Haritaya_Bölge_Çiz_İşlemleri/TC_0013_07_Haritaya_Bölge_Çiz_İşlemi_Sil.java
@@ -14,7 +14,10 @@ import testAutomations.Kullanici;
 public class TC_0013_07_Haritaya_Bölge_Çiz_İşlemi_Sil extends testAutomations.TestBase {
     private String baseUrl;
 
-    @FindBy(xpath = "//body/div[@id='kt_quick_panel']/div[1]/ul[1]/li[2]/a[1]/i[1]")
+//    @FindBy(id = ("kt_quick_panel_toggle"))
+//    public WebElement ok;
+
+    @FindBy(css = "#kt_quick_panel > div.quick-panel-toggle > ul:nth-child(2) > li:nth-child(2)")
     public WebElement aramaİslemleri;
 
     @FindBy(css = "#kt_quick_panel_2 > div.navi.navi-icon-circle.navi-spacer-x-0 > a:nth-child(2)")
@@ -56,6 +59,8 @@ public class TC_0013_07_Haritaya_Bölge_Çiz_İşlemi_Sil extends testAutomation
         }catch (Exception e){
             Logger.warn("Kullanıcı login olamadı.");
         }
+//        Fwait.until(ExpectedConditions.visibilityOf(ok));
+//        ok.click();
 
         Fwait.until(ExpectedConditions.visibilityOf(aramaİslemleri));
         aramaİslemleri.click();
